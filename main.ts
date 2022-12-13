aa53e8441a5bdde3249b4b34a738e83cbf05da5d
lukierose <120498257+lukierose@users.noreply.github.com>
2022-12-13 08:57:55 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,37 @@
+irVS1838.onIrButton(IrButton.Num1, IrButtonAction.Pressed, function () {
+    basic.showNumber(1)
+})
+irVS1838.onIrButton(IrButton.Up, IrButtonAction.Pressed, function () {
+	
+})
+irVS1838.onIrButton(IrButton.Num8, IrButtonAction.Pressed, function () {
+    basic.showNumber(8)
+})
+irVS1838.onIrButton(IrButton.Num0, IrButtonAction.Pressed, function () {
+    basic.showNumber(0)
+})
+irVS1838.onIrButton(IrButton.Num9, IrButtonAction.Pressed, function () {
+    basic.showNumber(9)
+})
+irVS1838.onIrButton(IrButton.Num6, IrButtonAction.Pressed, function () {
+    basic.showNumber(6)
+})
+irVS1838.onIrButton(IrButton.Num4, IrButtonAction.Pressed, function () {
+    basic.showNumber(4)
+})
+irVS1838.onIrButton(IrButton.Num3, IrButtonAction.Pressed, function () {
+    basic.showNumber(3)
+})
+irVS1838.onIrButton(IrButton.Num2, IrButtonAction.Pressed, function () {
+    basic.showNumber(2)
+})
+irVS1838.onIrButton(IrButton.Num7, IrButtonAction.Pressed, function () {
+    basic.showNumber(7)
+})
+irVS1838.onIrButton(IrButton.Num5, IrButtonAction.Pressed, function () {
+    basic.showNumber(5)
+})
+irVS1838.connectIrReceiver(DigitalPin.P1)
+basic.forever(function () {
+	
+})
